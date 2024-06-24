fa4d4778625cad089542f21e2a9aa6a18536085f
JardiinWD <Pecorillaalessandro@gmail.com>
2024-06-24 16:07:06 +0200

    Initialize environment variables for postgres and Config Service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,44 @@
+// ======== MODULES =========
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+// ======== CONTROLLERS =========
 import { AppController } from './app.controller';
+// ======== SERVICES =========
 import { AppService } from './app.service';
+// ======== ENTITIES =========
+// ======== CONFIG =========
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 
 @Module({
-  imports: [],
+  imports: [
+    // Load environment variables
+    ConfigModule.forRoot({
+      isGlobal: true, // Set `true` for global configuration
+      envFilePath: `.env`, // Set the path to the .env file based on the NODE_ENV
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService], // Inject the ConfigService in the AppModule
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres', // SQLite database type
+          host: configService.get<string>('DATABASE_HOST'), // Database host from the .env file
+          database: configService.get<string>('DATABASE_NAME'), // Database name from the .env file
+          port: configService.get<number>('DATABASE_PORT'), // Database port from the .env file
+          username: configService.get<string>('DATABASE_USER'), // Database user from the .env file
+          password: configService.get<string>('DATABASE_PASSWORD'), // Database password from the .env file
+          entities: [], // Entities to be stored in the database (Users and Reports)
+          synchronize: true, // Set `true` to synchronize the database schema with the entities
+          ssl: true, // Set `true` to enable SSL
+          connection: {
+            // Options allow to connect to the database using a connection string
+            options: `project=${configService.get<string>('DATABASE_ENDPOINT_ID')}`
+          }
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+// ====== IMPORTS ======
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app
+    .listen(process.env.NEST_PORT || 3375)
+    .then(() => {
+      console.log(`Application is running on port: ${process.env.NEST_PORT}`);
+    }).catch((err) => {
+      console.error("Error while starting application: ", err);
+    });
 }
 bootstrap();
