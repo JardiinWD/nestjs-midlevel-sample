6037780c90f3068b318432c14abbc5d935260d65
JardiinWD <Pecorillaalessandro@gmail.com>
2024-07-06 15:01:54 +0200

    Completed Files logic

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import { static as expose } from 'express';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Expose Public Folder
+  app.use(expose('public'));
   await app
     .listen(process.env.NEST_PORT || 3375)
     .then(() => {
